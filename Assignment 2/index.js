beb4d5c569ee30b69c8ea97cd8459044bbe114c3
Dhruval Dhameliya <dcdhameliya@gmail.com>
2023-10-22 21:53:43 -0400

    Assignment 2 Final

--- Assignment 2/index.js
+++ Assignment 2/index.js
@@ -1,3 +1,4 @@
+// Function to reverse a string
 function reverse(str) {
     var reversed = "";
     for (var i = str.length - 1; i >= 0; i--) {
@@ -7,11 +8,26 @@ function reverse(str) {
     return reversed;
 }
 
-function palindrome(str) {
-    var reversed = reverse(str);
-    return reversed === str;
+
+// Function to check if a series of number is a palindrome
+function palindrome(number) {
+    if (number < 0 || (number % 10 === 0 && number !== 0)) {
+        return false;
+    }
+
+    let reversedNumber = 0;
+    let originalNumber = number;
+
+    while (number > 0) {
+        const digit = number % 10;
+        reversedNumber = reversedNumber * 10 + digit;
+        number = Math.floor(number / 10);
+    }
+
+    return originalNumber == reversedNumber;
 }
 
+// Function to count a total of bill
 function totalBill(bill,tip) {
     return bill + tip*bill/100;
 }
